8bdde0c2ebe7b45651b98dac6e6d81bd82baad7d
David W. Rankin Jr <david@bohemians.us>
2019-07-22 00:45:42 -0400

    We do need this file, not sure if we need the inports

--- Source/include/CocoaAsyncSocket.h
+++ Source/include/CocoaAsyncSocket.h
@@ -14,5 +14,5 @@ FOUNDATION_EXPORT double cocoaAsyncSocketVersionNumber;
 //! Project version string for CocoaAsyncSocket.
 FOUNDATION_EXPORT const unsigned char cocoaAsyncSocketVersionString[];
 
-#import <include/GCDAsyncSocket.h>
-#import <include/GCDAsyncUdpSocket.h>
+// #import <include/GCDAsyncSocket.h>
+// #import <include/GCDAsyncUdpSocket.h>
